799dc1e6b17a3ae3e5212b8c1418b5c6b93ab130
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-01-23 21:10:06 +0100

    displaying user list works

--- klient.c
+++ klient.c
@@ -7,198 +7,217 @@
 #include <stdlib.h>
 
 typedef struct msgbuf{
-  long type;
-  int cmd;
-  char nick[10];
-  char text[256];
-  char date[30];
-  int pid;
-  int status;
+    long type;
+    int cmd;
+    char nick[10];
+    char text[256];
+    char date[30];
+    int pid;
+    int status;
 } msgbuf;
 
-int main() {
-  int msgid, result, condition;
-  char nick[10];
-  char text[256];
-  
-  int choice = 1, choice1 = 1, choice2 = 1, groupChoice;
-  const char *group[3] = {"pierwsza", "druga", "trzecia"};
+void displayUserList(int *msgid, msgbuf *to_send, msgbuf *received, int *result);
 
-  msgid = msgget(15071410, IPC_CREAT | 0644); 
-  if(msgid == -1) {
-    perror("Utworzenie kolejki komunikatów");
-    exit(1);
-  }  
-  
-  msgbuf to_send, recieved;
-  
-  to_send.type = 1;
-  to_send.pid = getpid();
-  
-  while(choice) {
-    //log in
-    do {
-      printf("Witaj! Podaj swoją nazwę użytkownika: ");
-      scanf("%s", nick);
-      strcpy(to_send.nick, nick);
-      
-      to_send.cmd = 1;
-      
-      //sending username
-      result = msgsnd(msgid, &to_send, sizeof(to_send), 0);
-      if(result == -1) {
+int main() {
+    int msgid, result, condition;
+    char nick[10];
+    char text[256];
+    
+    int choice = 1, choice1 = 1, choice2 = 1, groupChoice;
+    const char *group[3] = {"pierwsza", "druga", "trzecia"};
+
+    msgid = msgget(15071410, IPC_CREAT | 0644); 
+    if(msgid == -1) {
+        perror("Utworzenie kolejki komunikatów");
+        exit(1);
+    }    
+    
+    msgbuf to_send, received;
+    
+    to_send.type = 1;
+    to_send.pid = getpid();
+    
+    while(choice) {
+        //log in
+        do {
+	    printf("Witaj! Podaj swoją nazwę użytkownika: ");
+            scanf("%s", nick);
+            strcpy(to_send.nick, nick);
+            
+            to_send.cmd = 1;
+            
+            //sending username
+            result = msgsnd(msgid, &to_send, sizeof(to_send), 0);
+            if(result == -1) {
+		perror("Wysyłanie elementu");
+		exit(1);
+            }
+            
+	    //response after sending username
+            result = msgrcv(msgid, &received, sizeof(received), getpid(), 0);
+            if(result == -1) {
+		perror("Odbieranie elementu");
+	    } else {
+		condition = received.status;
+		switch(condition) {
+		    case 0:
+			printf("Wszystko ok\n");
+			break;
+		    case 1:
+			printf("Taki nick już istnieje\n");
+			break;
+		    case 2:
+			printf("Jesteś już zalogowany\n");
+			condition = 0;
+			break;
+			case 3:
+			printf("Maksymalna liczba użytkowników. Wpisz 0 aby wyjść\n");
+			scanf("%d", &choice2);
+			if(choice2 == 0) choice = 0;	        
+			condition = 0;
+			choice1 = 0;
+			break;
+		    default:
+			printf("Oops, coś poszło nie tak\n");
+			break;
+		}
+            }
+        } while (condition != 0);
+        
+        //displaying menu
+        while(choice1) {
+            system("clear");
+            printf("Witaj %s! Oto Twoje możliwości:\n1. wyświetl użytkowników\n2. wyświetl grupy\n3. zmień nazwę użytkownika\n4. zapisz się do grupy\n5. wypisz się z grupy\n6. wyświetl czat\n7. wyślij wiadomość do grupy\n8. wyślij wiadomość prywatną\n9. wyloguj się\n0. zakończ\nWybierz jedną: ", nick);
+            scanf("%d", &choice1);
+            printf("\n");
+            
+            switch(choice1) {
+	
+		case 1:    
+		    //sending request for user list
+		    to_send.cmd = 2;
+		    result = msgsnd(msgid, &to_send, sizeof(to_send), 0);
+		    if(result == -1) {
+			perror("Wysyłanie elementu");
+			exit(1);
+		    }
+		    
+		    //receiving user list
+		    printf("Lista użytkowników: ");
+		    result = msgrcv(msgid, &received, sizeof(received), getpid(), 0);
+		    if(result == -1) {
+			perror("Odbieranie elementu");
+		    } else {
+			printf("%s", received.text);
+		    }		    
+		    do {
+			printf("\nWybierz 0 aby wrócić do menu: ");
+			scanf("%d", &choice2);
+		    }
+		    while(choice2 != 0);
+		    break;
+		    
+		case 2:
+		    printf("Lista grup:\npierwsza\ndruga\ntrzecia\n");
+		    do {
+			printf("Wybierz 0 aby wrócić do menu: ");
+			scanf("%d", &choice2);
+		    }
+		    while(choice2 != 0);
+		    break;
+		    
+		case 3:
+		    printf("Wprowadź nową nazwę użytkownika: ");
+		    scanf("%s", nick);
+		    break;
+		    
+		case 4:
+		    printf("Wybierz grupę do które chcesz się zapisać:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
+		    do {
+			scanf("%d", &groupChoice);
+		    } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
+		    break;
+		    
+		case 5:
+		    printf("Wybierz grupę z której chcesz się wypisać:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
+		    do {
+			scanf("%d", &groupChoice);
+		    } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
+		    break;
+		    
+		case 6:
+		    printf("Czat:\ndata: 18.01.2014\tod: nieznajomy\t treść: elo elo\n");
+		    do {
+			printf("Wybierz 0 aby wrócić do menu: ");
+			scanf("%d", &choice2);
+		    }
+		    while(choice2 != 0);
+		    break;
+		    
+		case 7:
+		    printf("Wybierz grupę do której chcesz wysłać wiadomość:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
+		    do {
+			scanf("%d", &groupChoice);
+		    } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
+		    printf("Podaj wiadomość: ");
+		    scanf("%s", text);
+		    printf("Wiadomość została wysłana!\n");
+		    do {
+			printf("Wybierz 0 aby wrócić do menu: ");
+			scanf("%d", &choice2);
+		    }
+		    while(choice2 != 0);
+		    break;
+		    
+		case 8:
+		    printf("Wybierz użytkownika do którego chcesz wysłać wiadomość:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
+		    do {
+			scanf("%d", &groupChoice);
+		    } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
+		    printf("Podaj wiadomość: ");
+		    scanf("%s", text);
+		    printf("Wiadomość została wysłana!\n");
+		    do {
+			printf("Wybierz 0 aby wrócić do menu: ");
+			scanf("%d", &choice2);
+		    }
+		    while(choice2 != 0);
+		    break;
+		    
+		case 9:
+		    choice1 = 0;
+		    break;
+		    
+		case 0:
+		    choice1 = 0;
+		    choice = 0;
+		default:
+		    break;
+            }
+        }
+    }
+    
+    msgctl(msgid, IPC_RMID, 0);	
+}
+
+void displayUserList(int *msgid, msgbuf *to_send, msgbuf *received, int *result) {
+    to_send->cmd = 2;
+    
+    printf("msgid %d, result %d\n", *msgid, *result);
+    
+    *result = msgsnd(*msgid, &to_send, sizeof(to_send), 0);
+    if(*result == -1) {
 	perror("Wysyłanie elementu");
 	exit(1);
-      }
-      
-      //response after sending username
-      result = msgrcv(msgid, &recieved, sizeof(recieved), getpid(), 0);
-      if(result == -1) {
-	perror("Odbieranie elementu");
-      } else {
-	condition = recieved.status;
-	switch(condition) {
-	  case 0:
-	    printf("Wszystko ok\n");
-	    break;
-	  case 1:
-	    printf("Taki nick już istnieje\n");
-	    break;
-	  case 2:
-	    printf("Jesteś już zalogowany\n");
-	    condition = 0;
-	    break;
-	  case 3:
-	    printf("Maksymalna liczba użytkowników. Wpisz 0 aby wyjść\n");
-	    scanf("%d", &choice2);
-	    if(choice2 == 0) choice = 0;	    
-	    condition = 0;
-	    choice1 = 0;
-	    break;
-	  default:
-	    printf("Oops, coś poszło nie tak\n");
-	    break;
-	}
-      }
-    } while (condition != 0);
-    
-    //displaying menu
-    while(choice1) {
-      system("clear");
-      printf("Witaj %s! Oto Twoje możliwości:\n1. wyświetl użytkowników\n2. wyświetl grupy\n3. zmień nazwę użytkownika\n4. zapisz się do grupy\n5. wypisz się z grupy\n6. wyświetl czat\n7. wyślij wiadomość do grupy\n8. wyślij wiadomość prywatną\n9. wyloguj się\n0. zakończ\nWybierz jedną: ", nick);
-      scanf("%d", &choice1);
-      printf("\n");
-      
-      switch(choice1) {
-	
-	case 1:
-	  printf("Lista użytkowników: ");
-	  do {
-	    printf("Wybierz 0 aby wrócić do menu: ");
-	    scanf("%d", &choice2);
-	  }
-	  while(choice2 != 0);
-	  break;
-	  
-	case 2:
-	  printf("Lista grup:\npierwsza\ndruga\ntrzecia\n");
-	  do {
-	    printf("Wybierz 0 aby wrócić do menu: ");
-	    scanf("%d", &choice2);
-	  }
-	  while(choice2 != 0);
-	  break;
-	  
-	case 3:
-	  printf("Wprowadź nową nazwę użytkownika: ");
-	  scanf("%s", nick);
-	  break;
-	  
-	case 4:
-	  printf("Wybierz grupę do które chcesz się zapisać:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
-	  do {
-	    scanf("%d", &groupChoice);
-	  } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
-	  break;
-	  
-	case 5:
-	  printf("Wybierz grupę z której chcesz się wypisać:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
-	  do {
-	    scanf("%d", &groupChoice);
-	  } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
-	  break;
-	  
-	case 6:
-	  printf("Czat:\ndata: 18.01.2014\tod: nieznajomy\t treść: elo elo\n");
-	  do {
-	    printf("Wybierz 0 aby wrócić do menu: ");
-	    scanf("%d", &choice2);
-	  }
-	  while(choice2 != 0);
-	  break;
-	  
-	case 7:
-	  printf("Wybierz grupę do której chcesz wysłać wiadomość:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
-	  do {
-	    scanf("%d", &groupChoice);
-	  } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
-	  printf("Podaj wiadomość: ");
-	  scanf("%s", text);
-	  printf("Wiadomość została wysłana!\n");
-	  do {
-	    printf("Wybierz 0 aby wrócić do menu: ");
-	    scanf("%d", &choice2);
-	  }
-	  while(choice2 != 0);
-	  break;
-	  
-	case 8:
-	  printf("Wybierz użytkownika do którego chcesz wysłać wiadomość:\n1. %s\n2. %s\n3. %s\n", group[0], group[1], group[2]);
-	  do {
-	    scanf("%d", &groupChoice);
-	  } while (groupChoice != 1 && groupChoice != 2 && groupChoice != 3);
-	  printf("Podaj wiadomość: ");
-	  scanf("%s", text);
-	  printf("Wiadomość została wysłana!\n");
-	  do {
-	    printf("Wybierz 0 aby wrócić do menu: ");
-	    scanf("%d", &choice2);
-	  }
-	  while(choice2 != 0);
-	  break;
-	  
-	case 9:
-	  choice1 = 0;
-	  break;
-	  
-	case 0:
-	  choice1 = 0;
-	  choice = 0;
-	default:
-	  break;
-      }
     }
-  }
-  
-  msgctl(msgid, IPC_RMID, 0);	
-
-      
-/*  
-  int msgid;
-  struct msgbuf message;
-  
-  msgid = msgget(15071410, IPC_CREAT | 0644); 
-  if(msgid == -1) {
-    perror("Utworzenie kolejki komunikatów");
-  }
-  printf("%d", msgid);
-  
-  strcpy(message.text, "hello");
-  message.type = 1;
-  
-  if(msgsnd(msgid, &message, sizeof(message.text), 0) == -1) {
-    perror("Wysyłanie elementu");
-  }
-  msgctl(15071410, IPC_RMID, 0);
-*/  
+    
+    printf("wysłano\n");
+    
+    *result = msgrcv(*msgid, &received, sizeof(received), getpid(), 0);
+    if(*result == -1) {
+	perror("Odbieranie elementu");
+    } else {
+	printf("%s", received->text);
+    }
+    printf("odebrano");
 }
--- serwer.c
+++ serwer.c
@@ -6,108 +6,123 @@
 #include <sys/msg.h>
 
 typedef struct msgbuf {
-  long type;
-  int cmd;
-  char nick[10];
-  char text[256];
-  char date[30];
-  int pid;
-  int status;
+    long type;
+    int cmd;
+    char nick[10];
+    char text[256];
+    char date[30];
+    int pid;
+    int status;
 } msgbuf;
 
 typedef struct logged {
-  char nick[10];
-  int pid;
+    char nick[10];
+    int pid;
 } logged;
 
+void generateUserList(logged loggedArray[18], char *text);
+
 int main() {
-  int msgid, result, type, loop = 1;
-  int i;
-  
-  msgbuf to_send, recieved;
-  logged loggedArray[18];
-  
-  for(i=0; i<18; i++) {
-    loggedArray[i].pid = 0;
-    strcpy(loggedArray[i].nick, "");
-  }
-//   //test - zapełnij prawie całą tablicę
-//   for(i=0; i<15; i++) {
-//       loggedArray[i].pid = i+1;
-//   }
-  
-//   printf("test");
-  
-  msgid = msgget(15071410, IPC_CREAT | 0644); 
-  if(msgid == -1) {
-    perror("Utworzenie kolejki komunikatów");
-    exit(1);
-  }
-  
-  printf("msgid: %d\n", msgid);
-  
-  while(loop == 1) {
+    int msgid, result, type, loop = 1;
+    int i;
     
-    result = msgrcv(msgid, &recieved, sizeof(recieved), 1, 0);
-    if(result == -1) {
-      perror("Odbieranie elementu");
-      exit(1);
-    } 
-    else {
-      printf("%s", recieved.nick);
-      to_send.type = recieved.pid;
-      
-      switch(recieved.cmd) {
-	case 1:
-	  to_send.cmd = 1;
-	  int userInArray = returnUserInArray(loggedArray, recieved.nick, recieved.pid);
-// 	  printf("User in array: %d\n\n", userInArray);
-	  if(userInArray < 0) {
-	    to_send.status = abs(userInArray);
-	  } else {
-	    to_send.status = 0;
-	    strcpy(loggedArray[userInArray].nick, recieved.nick);
-	    loggedArray[userInArray].pid = recieved.pid;
-	  }
-// 	  for(i=0; i<18; i++) printf("pid value: %d, nick: %s\n ", loggedArray[i].pid, loggedArray[i].nick);
-// 	  printf("type: %ld, cmd: %d. status: %d\n", to_send.type, to_send.cmd, to_send.status);
-	  break;
-	  
-	default:
-	  printf("Something went wrong");
-	  break;
-      }
+    msgbuf to_send, received;
+    logged loggedArray[18];
+    
+    for(i=0; i<18; i++) {
+        loggedArray[i].pid = 0;
+        strcpy(loggedArray[i].nick, "");
+    }
+//     //test - zapełnij prawie całą tablicę
+//     for(i=0; i<15; i++) {
+//             loggedArray[i].pid = i+1;
+//     }
+    
+//     printf("test");
+    
+    msgid = msgget(15071410, IPC_CREAT | 0644); 
+    if(msgid == -1) {
+        perror("Utworzenie kolejki komunikatów");
+        exit(1);
     }
     
-    result = msgsnd(msgid, &to_send, sizeof(to_send), 0);
-    if(result == -1) {
-      perror("Wysyłanie elementu");
-      exit(1);
-    } else (printf("wysłano odpowiedź"));
-  }
-  
-  return 0;
+    printf("msgid: %d\n", msgid);
+    
+    while(loop == 1) {
+        
+        result = msgrcv(msgid, &received, sizeof(received), 1, 0);
+        if(result == -1) {
+            perror("Odbieranie elementu");
+            exit(1);
+        } 
+        else {
+//             printf("%s", received.nick);
+            to_send.type = received.pid;
+            
+            switch(received.cmd) {
+	case 1:
+	    to_send.cmd = 1;
+	    int userInArray = returnUserInArray(loggedArray, received.nick, received.pid);
+// 	    printf("User in array: %d\n\n", userInArray);
+	    if(userInArray < 0) {
+	        to_send.status = abs(userInArray);
+	    } else {
+	        to_send.status = 0;
+	        strcpy(loggedArray[userInArray].nick, received.nick);
+	        loggedArray[userInArray].pid = received.pid;
+	    }
+// 	    for(i=0; i<18; i++) printf("pid value: %d, nick: %s\n ", loggedArray[i].pid, loggedArray[i].nick);
+// 	    printf("type: %ld, cmd: %d. status: %d\n", to_send.type, to_send.cmd, to_send.status);
+	    break;
+	    
+	case 2:
+// 	    printf("weszło w case 2\n");
+	    to_send.cmd = 2;
+	    strcpy(to_send.text, "");
+	    for(i=0; i<18; i++) {
+		if(strcmp(loggedArray[i].nick, "")) {
+		    strcat(to_send.text, loggedArray[i].nick);
+		    strcat(to_send.text, "; ");
+		}		
+	    }
+	    printf("%s\n", to_send.text);
+	    break;	    
+	    
+	default:
+	    printf("Something went wrong");
+	    break;
+            }
+        }
+        
+        result = msgsnd(msgid, &to_send, sizeof(to_send), 0);
+        if(result == -1) {
+            perror("Wysyłanie elementu");
+            exit(1);
+        } else (printf("wysłano odpowiedź"));
+    }
+    
+    return 0;
 }
 
 int returnUserInArray(logged loggedArray[18], char nick[10], int pid) {
-  int toReturn = -3;
-  int i;
-//   for(i=0; i<18; i++) printf("pid value: %d, nick: %s\n ", loggedArray[i].pid, loggedArray[i].nick);
-  for(i=0; i<18; i++) {
-//       printf("nick to insert %s\n", nick);
-      if(!strcmp(loggedArray[i].nick,nick)) {
+    int toReturn = -3;
+    int i;
+//     for(i=0; i<18; i++) printf("pid value: %d, nick: %s\n ", loggedArray[i].pid, loggedArray[i].nick);
+	for(i=0; i<18; i++) {
+//             printf("nick to insert %s\n", nick);
+            if(!strcmp(loggedArray[i].nick,nick)) {
 	if(loggedArray[i].pid == pid) {
-	  return -2;
+	    return -2;
 	}
 	return -1;
-      }
-  }
-  for(i=17; i>=0; i--) {
-//     printf("%lu", sizeof(loggedArray[i]));
-    if(loggedArray[i].pid == 0) {
-      return i;
+            }
     }
-  }
-  printf("\n");
-  return -3;
+    for(i=17; i>=0; i--) {
+//         printf("%lu", sizeof(loggedArray[i]));
+        if(loggedArray[i].pid == 0) {
+            return i;
+        }
+    }
+    printf("\n");
+    return -3;
 }
